4207dfd77bddfde324acac7a85fbaec521fe0255
NegK01 <mauricio.or99@gmail.com>
2023-11-24 11:14:37 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# from tkinter import *
+# (Importa casi todo el modulo de tkinter y nos ahorra poner "tkinter."Label)
+Import tkinter as tk # Tedioso poner "tk."Label, pero nos ayuda a no confundirnos
+
+from tkinter import PhotoImage # Importar imagenes
+from time import * #
+from threading import *
+
+
+class Pomodoro_Timer:
+
+    def __init__(self):
+        self.ventana = Tk()
+        self.ventana.mainloop()
+
+
+
+Pomodoro_Timer()
