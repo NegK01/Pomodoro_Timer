b6a8f9eb0d5a3c23222926cc15676709cab18e3c
NegK01 <mauricio.or99@gmail.com>
2023-11-24 11:54:28 -0600

    Warap

--- main.py
+++ main.py
@@ -2,16 +2,18 @@
 # (Importa casi todo el modulo de tkinter y nos ahorra poner "tkinter."Label)
 Import tkinter as tk # Tedioso poner "tk."Label, pero nos ayuda a no confundirnos
 
-from tkinter import PhotoImage # Importar imagenes
-from time import * #
-from threading import *
-
+from tkinter import PhotoImage # Importa imagenes
+from time import * # Importa todo aquello relacionado al tiempo
+from threading import * # Nos permite ejecutar hilos que realicen tareas simultaneamente
+                      # ↑ Ojo, estos hilos son distintos, los usaremos para que no
+                      # se detenga el pomodoro cuando movemos la ventana u otra funcion
 
 class Pomodoro_Timer:
 
     def __init__(self):
         self.ventana = Tk()
         self.ventana.mainloop()
+
 
 
 
